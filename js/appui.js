d02800fc697e1834ea1f0f901c9ad596e2bdbe32
Michael Wellner <michael.wellner@de.ibm.com>
2015-03-06 23:54:50 +0100

    Small improvement.

--- js/appui.js
+++ js/appui.js
@@ -9,95 +9,25 @@ App.Radiobox = require("./components/radiobox");
 App.Button = require("./components/button");
 App.Listbox = require("./components/listbox");
 
-module.exports = App
-
-/**
-  * Flat UI
-  */
 /*
-App.Titlebar = React.createClass({
-  getDefaultProps: function() {
-    return { 
-      layout:   "horizontal",
-      size:     "20",
-      justify:  "space-between",
-      align:    "center"
-    }
-  },
-  
-  render: function() {
-    var className = this.props.className;
-    
-    return (
-        <App.Panel { ...this.props } className={ extendClassNameWith("titlebar", className) }>{ this.props.children }</App.Panel>
-      );
-  }
-})
+ ...field
+ Form
+ FieldGroup
+ DropdownButton
+ ToggleButton
+ 
+ Table
+ TreeTable
+ 
+ Progressbar
+ Slider (Range)
+ 
+ Date Picker
+ Time Picker
+ Tabs
+ 
+ File Upload?
+ */
+ 
 
-App.Text = React.createClass({
-  render: function() {
-    return (
-        <div { ...this.props }>{ this.props.children }</div>
-      );
-  }
-})
-
-App.Toolbar = React.createClass({
-  getDefaultProps: function() {
-    return {
-      layout:   "horizontal",
-      size:     "100",
-      justify:  "start",
-      align:    "center"
-    }
-  },
-  
-  render: function() {
-    var className = this.props.className;
-    
-    return (
-        <App.Panel { ...this.props } className={ extendClassNameWith("toolbar", className) }>{ this.props.children }</App.Panel>
-      );
-  }
-})
-
-App.Form = React.createClass({
-  render: function() {
-    return (<form { ...this.props }>{ this.props.children }</form>);
-  }
-})
-
-App.Form.Textfield = React.createClass({
-  
-  getDefaultProps: function() {
-    return {
-      layout:   "horizontal",
-      justify:  "space-between",
-      align:    "center",
-      size:     "auto"
-    }
-  },
-  
-  render: function() {
-    var 
-      className = extendClassNameWith("control", this.props.className),
-      label = undefined;
-  
-    if (this.props.label) {
-      label = <label forName={ this.props.id } ratio="1">{ this.props.label }</label>
-    }
-    
-    return (
-        <App.Panel layout={ this.props.layout } className={ className } ratio={ this.props.ratio } size={ this.props.size }>
-          { label }
-          <input type="text" name={ this.props.id } defaultValue={ this.props.value } ratio="4" onChange={ this._onChange } />
-        </App.Panel>
-      );
-  },
-  
-  _onChange: function(event) {
-    this.props.onChange(event.target.value);
-  }
-})
-
-module.exports = App;*/
+module.exports = App
